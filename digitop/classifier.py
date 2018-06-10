d4eff372dc1b884cd41a6628fc9155a9d44179f1
Withington <lucy.withington@outlook.com>
2018-06-10 17:00:24 +0100

    Added hyperparameter searching.

--- digitop/classifier.py
+++ digitop/classifier.py
@@ -29,18 +29,21 @@ def evaluate_classifier():
     model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
 
-    adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    for use_adam in [True, False]:
+        for lr in [0.001, 0.01] :
+            adam = Adam(lr=lr, beta_1=0.9, beta_2=0.999)
+            sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+            model.compile(loss='categorical_crossentropy', optimizer=adam if use_adam else sgd, metrics=['accuracy'])
 
-    tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
-                          write_graph=True, write_images=False)
-    history = model.fit(x_train, y_train,
-                        epochs=20,
-                        batch_size=128,
-                        callbacks=[tensorboard])
+            param_string = ''.join(['lr=', str(lr),',opt_is_adam=',str(use_adam)])
+            tensorboard = TensorBoard(log_dir='./logs/'+param_string, histogram_freq=0,
+                                  write_graph=True, write_images=False)
+            history = model.fit(x_train, y_train,
+                                epochs=20,
+                                batch_size=128,
+                                callbacks=[tensorboard])
 
-    score = model.evaluate(x_test, y_test, batch_size=128)
+            score = model.evaluate(x_test, y_test, batch_size=128)
     return score, history
 
 def plot_history(history):
